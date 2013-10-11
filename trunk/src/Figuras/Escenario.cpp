3e345952d6c0aa0998abae166cdb5e1f9e86942f
collinetjorge@gmail.com <collinetjorge@gmail.com@b9ed9b76-5c66-3f4a-9d64-aeaa211ddf59>
2013-10-11 05:25:43 +0000

    termino animacion de cangrejo

--- trunk/src/Figuras/Escenario.cpp
+++ trunk/src/Figuras/Escenario.cpp
@@ -7,11 +7,12 @@
 
 #include "Escenario.h"
 
-Escenario::Escenario(myWindow* ventana):Figura(ventana),cangrejo1 (ventana), piedra1 (ventana),planta1(ventana),piso(ventana) {
+Escenario::Escenario(myWindow* ventana):Figura(ventana),cangrejo1 (ventana), piedra1 (ventana),planta1(ventana),piso(ventana),pos_cangrejo(0,0,1.3f) {
 	figuras.push_back(&cangrejo1);
 	figuras.push_back(&piedra1);
 	figuras.push_back(&planta1);
 	figuras.push_back(&piso);
+	vuelta_cangrejo = false;
 }
 
 Escenario::~Escenario() { }
@@ -19,7 +20,7 @@ Escenario::~Escenario() { }
 void Escenario::renderizar(glm::mat4 model_matrix) {
 	this->piso.renderizar(model_matrix);
 
-	glm::mat4 model_matrix_cangrejo1 = glm::translate(model_matrix, glm::vec3 (0.0f,0.0f,1.3f));
+	glm::mat4 model_matrix_cangrejo1 = glm::translate(model_matrix, pos_cangrejo);
 	this->cangrejo1.renderizar(model_matrix_cangrejo1);
 
 	glm::mat4 model_matrix_planta1 = glm::translate(model_matrix, glm::vec3 (-5.0f,5.0f,0.9f));
@@ -28,7 +29,22 @@ void Escenario::renderizar(glm::mat4 model_matrix) {
 	glm::mat4 model_matrix_piedra1 = glm::translate(model_matrix,glm::vec3(5,5,0));
 	this->piedra1.renderizar(model_matrix_piedra1);
 }
-void Escenario::animar(){
+void Escenario::animar() {
+	std::cout<<"pos x cangrejo"<<pos_cangrejo.x<<std::endl;
+
+	if (vuelta_cangrejo == true) {
+		pos_cangrejo.x-=0.2f;
+		if( pos_cangrejo.x<=-10.0f ) {
+			vuelta_cangrejo = false;
+		}
+	} else {
+		pos_cangrejo.x+=0.2f;
+		if( pos_cangrejo.x>=10.0f ) {
+			vuelta_cangrejo = true;
+		}
+	}
+	std::cout<<"pos x cangrejo"<<pos_cangrejo.x<<std::endl;
+
 	for (unsigned int i = 0; i<figuras.size(); ++i) {
 		figuras[i]->animar();
 	}
