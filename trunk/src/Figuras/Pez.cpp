90f8d76da63a6814b8352d88281ff2f8d2b20f8b
collinetjorge@gmail.com <collinetjorge@gmail.com@b9ed9b76-5c66-3f4a-9d64-aeaa211ddf59>
2013-10-14 20:37:43 +0000

    merge Pez.cpp

--- trunk/src/Figuras/Pez.cpp
+++ trunk/src/Figuras/Pez.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "Pez.h"
+#include <vector>
+
 
 // ******************************* ALETA *******************************
 AletaPez::AletaPez(myWindow* ventana) :Figura(ventana) {
@@ -16,7 +18,6 @@ void AletaPez::renderizar(glm::mat4 model_matrix) {
 	model_matrix = glm::scale(model_matrix,glm::vec3(0.5,0.5,1.5));
 	Figura::renderizar(model_matrix);
 }
-
 // PARA HACER !!!
 Curva* AletaPez::crear_curva_trayectoria () {
 	return (new Curva(glm::vec3(0.0,0.0,0.0),glm::vec3(0.0,0.0,1.0)));
@@ -38,7 +39,6 @@ std::vector<glm::mat4> AletaPez::crear_transformaciones () {
 Superficie* AletaPez::crear_superficie (myWindow* ventana) {
 	return (new Cubo (ventana));
 }
-
 // ******************************* COLA ********************************
 ColaPez::ColaPez(myWindow* ventana) :Figura(ventana) {
 	this->mi_superficie = this->crear_superficie (ventana);
@@ -77,8 +77,12 @@ TorsoPez::TorsoPez(myWindow* ventana) :Figura(ventana) {
 TorsoPez::~TorsoPez() { }
 void TorsoPez::renderizar(glm::mat4 model_matrix) {
 
-}
+	//model_matrix = glm::rotate(model_matrix, 45.0f, glm::vec3(0.0,1.0,0.0));
+	//model_matrix = glm::rotate(model_matrix, 90.0f, glm::vec3(1.0,0.0,0.0));
 
+	model_matrix = glm::scale(model_matrix,glm::vec3(1.5,0.5,0.5));
+	this->myCube->render(model_matrix);
+}
 // PARA HACER !!!
 Curva* TorsoPez::crear_curva_trayectoria () {
 	return (new Curva(glm::vec3(0.0,0.0,0.0),glm::vec3(0.0,0.0,1.0)));
@@ -101,15 +105,56 @@ Superficie* TorsoPez::crear_superficie (myWindow* ventana) {
 	return (new Cubo (ventana));
 }
 
-// ******************************** PEZ ********************************
-Pez::Pez(myWindow* ventana) : Figura (ventana),aleta1(ventana),aleta2(ventana),cola(ventana),torso(ventana) {
+Pez::Pez(myWindow* ventana) :
+		Figura(ventana), aleta1(ventana), aleta2(ventana), cola(ventana), torso(ventana), m_pos(0,0,0) {
+	trayecto=NULL;
+	cont=0;
+	angX=0;
+	angZ=0;
 	this->mi_superficie = this->crear_superficie (ventana);
 }
 Pez::~Pez() {}
 void Pez::renderizar(glm::mat4 model_matrix) {
+	model_matrix = glm::translate(model_matrix,m_pos);
+	model_matrix = glm::rotate(model_matrix,angZ,glm::vec3(0,0,1));
 	aleta1.renderizar(model_matrix);
 	aleta2.renderizar(model_matrix);
 	cola.renderizar(model_matrix);
-	torso.renderizar(model_matrix);
-}
 
+	torso.renderizar(model_matrix);
+
+}
+void Pez::set_trayecto(std::vector<glm::vec3> puntos){
+	glm::vec3 centro (1,1,1);
+	glm::vec3 orientacion (1,1,1);
+	trayecto = new CurvaBSpline(puntos,centro,orientacion);
+}
+void Pez::animar() {
+	if (trayecto == NULL) {
+		std::cout << "TRAYECTO NO INICIADO";
+		return;
+	}
+	std::cout << "Cantidad de tramos: " << trayecto->cantidad_tramos() << " "<< std::endl;
+	glm::vec3 a = trayecto->dameTangente(cont, 0.001f);
+
+	// cont es el u , y avanzo al siguiente paso
+	cont += 0.01;
+	if (cont > trayecto->cantidad_tramos()) {
+		cont = 0;
+		angZ = 0;
+	}
+	glm::vec3 b = trayecto->dameTangente(cont, 0.001f);
+
+	// asigno siguiente posicion al pez
+	glm::vec3 nueva_pos = trayecto->damePunto(cont);
+	m_pos = nueva_pos;
+	std::cout << "trayecto:" << nueva_pos.x << " , " << nueva_pos.y << " , "<< nueva_pos.z << std::endl;
+
+	// calculo del angulo a sumar
+	float radianes = acos(glm::dot(a, b));
+	angZ += (radianes * 360) / (2 * 3.141592654f);
+	std::cout << "angulo de giroZ: " << angZ << std::endl;
+
+	//std::cout<<"prueba"<<acos(0)<<std::endl;
+	// me lo da en radianes
+}
