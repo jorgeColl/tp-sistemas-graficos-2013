53651a30d5708c3315d90eb0651df8268a7a7efe
collinetjorge <collinetjorge@gmail.com>
2013-10-14 20:32:36 +0000

    merge de Escenario.cpp

--- src/Figuras/Escenario.cpp
+++ src/Figuras/Escenario.cpp
@@ -9,11 +9,21 @@
 
 Escenario::Escenario(myWindow* ventana):Figura(ventana),pez1(ventana),cangrejo1 (ventana), piedra1 (ventana),planta1(ventana),piso(ventana),pos_cangrejo(0,0,1.3f) {
 	figuras.push_back(&cangrejo1);
-	figuras.push_back(&cangrejo1);
+	figuras.push_back(&pez1);
 	figuras.push_back(&piedra1);
 	figuras.push_back(&planta1);
 	figuras.push_back(&piso);
 	vuelta_cangrejo = false;
+
+	std::vector<glm::vec3> puntos;
+	puntos.push_back(glm::vec3(1,0,1));
+	puntos.push_back(glm::vec3(1,0,1));
+	puntos.push_back(glm::vec3(0,1,1));
+	puntos.push_back(glm::vec3(-1,0,1));
+	puntos.push_back(glm::vec3(0,-1,1));
+	//puntos.push_back(glm::vec3(3,1,1));
+	//puntos.push_back(glm::vec3(3.5,1,1));
+	pez1.set_trayecto(puntos);
 	this->mi_superficie = this->crear_superficie (ventana);
 }
 
@@ -25,16 +35,18 @@ void Escenario::renderizar(glm::mat4 model_matrix) {
 	this->pez1.renderizar(model_matrix);
 
 	glm::mat4 model_matrix_cangrejo1 = glm::translate(model_matrix, pos_cangrejo);
-	this->cangrejo1.renderizar(model_matrix_cangrejo1);
+	//this->cangrejo1.renderizar(model_matrix_cangrejo1);
 
 	glm::mat4 model_matrix_planta1 = glm::translate(model_matrix, glm::vec3 (-5.0f,5.0f,0.9f));
-	this->planta1.renderizar(model_matrix_planta1);
+	//this->planta1.renderizar(model_matrix_planta1);
 
 	glm::mat4 model_matrix_piedra1 = glm::translate(model_matrix,glm::vec3(5,5,0));
-	this->piedra1.renderizar(model_matrix_piedra1);
+	//this->piedra1.renderizar(model_matrix_piedra1);
 }
 void Escenario::animar() {
-	std::cout<<"pos x cangrejo"<<pos_cangrejo.x<<std::endl;
+
+	// INICIO de cosa sucia para mover el cangrejo
+	//std::cout<<"pos x cangrejo"<<pos_cangrejo.x<<std::endl;
 
 	if (vuelta_cangrejo == true) {
 		pos_cangrejo.x-=0.2f;
@@ -47,7 +59,8 @@ void Escenario::animar() {
 			vuelta_cangrejo = true;
 		}
 	}
-	std::cout<<"pos x cangrejo"<<pos_cangrejo.x<<std::endl;
+	//std::cout<<"pos x cangrejo"<<pos_cangrejo.x<<std::endl;
+	// FIN de cosa sucia
 
 	for (unsigned int i = 0; i<figuras.size(); ++i) {
 		figuras[i]->animar();
