bce8c48eef739174f993fa2de1dba823af38d6fb
collinetjorge@gmail.com <collinetjorge@gmail.com@b9ed9b76-5c66-3f4a-9d64-aeaa211ddf59>
2013-10-17 19:36:57 +0000

    mas plantas mas piedras

--- trunk/src/Figuras/Escenario.cpp
+++ trunk/src/Figuras/Escenario.cpp
@@ -7,19 +7,28 @@
 
 #include "Escenario.h"
 
-Escenario::Escenario(myWindow* ventana):Figura(ventana),pez1(ventana),cangrejo1 (ventana), piedra1 (ventana),planta1(ventana),piso(ventana) {
+Escenario::Escenario(myWindow* ventana):Figura(ventana),pez1(ventana),cangrejo1 (ventana),
+				piedra1(ventana), piedra2(ventana), piedra3(ventana), piedra4(ventana), piedra5(ventana),
+				planta1(ventana), planta2(ventana), planta3(ventana), planta4(ventana), planta5(ventana),
+				piso(ventana) {
 	pos_cangrejo1=glm::vec3 (0,0,1.3f);
 	pos_pez1 = glm::vec3 (-3,0,0.4f);
 
 	figuras.push_back(&cangrejo1);
 	figuras.push_back(&pez1);
 	figuras.push_back(&piedra1);
+	figuras.push_back(&piedra2);
+	figuras.push_back(&piedra3);
+	figuras.push_back(&piedra4);
+	figuras.push_back(&piedra5);
 	figuras.push_back(&planta1);
+	figuras.push_back(&planta2);
+	figuras.push_back(&planta3);
+	figuras.push_back(&planta4);
+	figuras.push_back(&planta5);
 	figuras.push_back(&piso);
 
-
 	std::vector<glm::vec3> puntos;
-
 	puntos.push_back(glm::vec3(0, 0, 0));
 	puntos.push_back(glm::vec3(0, 0, 0));
 	puntos.push_back(glm::vec3(1, 0, 0));
@@ -56,11 +65,32 @@ void Escenario::renderizar(glm::mat4 model_matrix) {
 	glm::mat4 model_matrix_cangrejo1 = glm::translate(model_matrix, pos_cangrejo1);
 	//this->cangrejo1.renderizar(model_matrix_cangrejo1);
 
-	glm::mat4 model_matrix_planta1 = glm::translate(model_matrix, glm::vec3 (-5.0f,5.0f,0.9f));
-	//this->planta1.renderizar(model_matrix_planta1);
-
-	glm::mat4 model_matrix_piedra1 = glm::translate(model_matrix,glm::vec3(5,5,0));
-	//this->piedra1.renderizar(model_matrix_piedra1);
+	bool RENDERIZAR_PLANTAS = false;
+	if(RENDERIZAR_PLANTAS) {
+		glm::mat4 model_matrix_planta1 = glm::translate(model_matrix,glm::vec3(-5.0f, 5.0f, 0.9f));
+		this->planta1.renderizar(model_matrix_planta1);
+		glm::mat4 model_matrix_planta2 = glm::translate(model_matrix,glm::vec3(-15.0f, 5.0f, 0.9f));
+		this->planta2.renderizar(model_matrix_planta2);
+		glm::mat4 model_matrix_planta3 = glm::translate(model_matrix,glm::vec3(-5.0f, 25.0f, 0.9f));
+		this->planta3.renderizar(model_matrix_planta3);
+		glm::mat4 model_matrix_planta4 = glm::translate(model_matrix,glm::vec3(-5.0f, -15.0f, 0.9f));
+		this->planta4.renderizar(model_matrix_planta4);
+		glm::mat4 model_matrix_planta5 = glm::translate(model_matrix,glm::vec3(13.0f, 13.0f, 0.9f));
+		this->planta5.renderizar(model_matrix_planta5);
+	}
+	bool RENDERIZAR_PIEDRAS = false;
+	if(RENDERIZAR_PIEDRAS) {
+		glm::mat4 model_matrix_piedra1 = glm::translate(model_matrix,glm::vec3(5,5,0));
+		this->piedra1.renderizar(model_matrix_piedra1);
+		glm::mat4 model_matrix_piedra2 = glm::translate(model_matrix,glm::vec3(-5,-5,0));
+		this->piedra1.renderizar(model_matrix_piedra2);
+		glm::mat4 model_matrix_piedra3 = glm::translate(model_matrix,glm::vec3(1,14,0));
+		this->piedra1.renderizar(model_matrix_piedra3);
+		glm::mat4 model_matrix_piedra4 = glm::translate(model_matrix,glm::vec3(12,-11,0));
+		this->piedra1.renderizar(model_matrix_piedra4);
+		glm::mat4 model_matrix_piedra5 = glm::translate(model_matrix,glm::vec3(7,8,0));
+		this->piedra1.renderizar(model_matrix_piedra5);
+	}
 }
 void Escenario::animar() {
 	for (unsigned int i = 0; i<figuras.size(); ++i) {
