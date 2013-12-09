2cc67b912bf0c062f44c9cd7a0eebfc224080303
daniel.fx.90@gmail.com <daniel.fx.90@gmail.com@b9ed9b76-5c66-3f4a-9d64-aeaa211ddf59>
2013-12-09 19:02:33 +0000

    Esfera llena buffers de tangentes y textura

--- trunk/inc/Superficies/Esfera.h
+++ trunk/inc/Superficies/Esfera.h
@@ -9,7 +9,10 @@ class Esfera : public Superficie {
 	
 	private:
 		static GLenum MODO;
-	
+		
+		void llenarBuffers (std::vector<float>* din_vertex_buffer, std::vector<float>* din_tangent_buffer, std::vector<float>* din_normal_buffer,
+							std::vector<float>* din_texture_buffer, const float radius, float theta, float phi);
+		
 	public:
 		Esfera (myWindow* passed_window, const float radius, const unsigned int loops, const unsigned int segmentsPerLoop);
 		~Esfera ();
